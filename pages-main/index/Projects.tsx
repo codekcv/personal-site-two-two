b93ca572d1952c2cf25d3954c2bda5e357410169
Christian Villamin <ChristianVillamin31@gmail.com>
2021-08-07 00:40:51 +0800

    feat(about) update info

--- pages-main/index/Projects.tsx
+++ pages-main/index/Projects.tsx
@@ -53,7 +53,7 @@ const Projects = (): JSX.Element => {
   })
 
   return (
-    <div className="mt-8" onClick={() => setIsOpen(!isOpen)}>
+    <div className="mt-2">
       <BobbleTitle inView={inView} isOpen={isOpen}>
         Project
       </BobbleTitle>
@@ -84,7 +84,7 @@ const Projects = (): JSX.Element => {
       />
 
       <Card
-        className="-mt-8"
+        className="mt-16  "
         inViewRef={refB}
         inView={inViewB}
         isOpen={x}
