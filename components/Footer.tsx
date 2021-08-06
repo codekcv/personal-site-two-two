015dda24830b4724da4a18d09d608082f08ad501
Christian Villamin <ChristianVillamin31@gmail.com>
2021-08-06 20:09:40 +0800

    feat(footer) simplify

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,30 +3,7 @@ const currentYear = new Date().getFullYear()
 const Footer = (): JSX.Element => (
   <div className="flex justify-center items-center w-full h-[64px] mt-[25vh]">
     <p className="text-center" color="#666">
-      © {currentYear} Christian Villamin | Built with&nbsp;
-      <a
-        href="https://www.gatsbyjs.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        gatsby
-      </a>
-      &nbsp;+&nbsp;
-      <a
-        href="https://chakra-ui.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        chakra
-      </a>
-      &nbsp;+&nbsp;
-      <a
-        href="https://www.framer.com/motion/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        motion
-      </a>
+      © {currentYear} Christian Villamin
     </p>
   </div>
 )
