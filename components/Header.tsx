b2e1c4656926c5c072a290a7bba30367c224b021
Christian Villamin <ChristianVillamin31@gmail.com>
2021-08-06 20:00:36 +0800

    feat(header) init

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+const menus = ['Home', 'Projects', 'Blogs', 'Contact'].map((menu) => (
+  <li key={menu} className="mx-2 list-none">
+    <span>{menu}</span>
+  </li>
+))
+
+const Header = (): JSX.Element => (
+  <div className="w-full my-8 justify-center items-center">
+    <div className="flex">{menus}</div>
+  </div>
+)
+
+export default Header
