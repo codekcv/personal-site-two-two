543116fcc122a87ff670a8d706c7537b0b6d6386
Christian Villamin <ChristianVillamin31@gmail.com>
2021-08-06 20:01:07 +0800

    feat(_app) layout init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
+  <div className="flex flex-col justify-center items-center min-h-screen max-w-screen-md mx-auto">
+    <div className="flex-1">
+      <Header />
+      <Component {...pageProps} />
+    </div>
+
+    <Footer />
+  </div>
 )
 
 export default MyApp
